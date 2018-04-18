6ead2beb6dcfe88714087edcd056eeb54ef8bc57
Max Mazzeschi <max.mazzeschi@gmail.com>
2018-04-18 14:52:20 +0200

    Image title is now burned at the top left

--- BG-from-NASA.ps1
+++ BG-from-NASA.ps1
@@ -53,12 +53,36 @@ RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters
 #         Get-ItemProperty -path "HKCU:\Control Panel\Desktop" 
 }
 
-$request =  "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
-$myurl = Invoke-WebRequest $request |
-ConvertFrom-Json |
-select hdurl  | select -expand hdurl
+[void] [System.Reflection.Assembly]::LoadWithPartialName('System.drawing') 
 
-Invoke-WebRequest -Uri $myurl -OutFile "C:\\temp\\pod.jpg"
-set-wallPaper("C:\temp\pod.jpg")
+$sourcePath = "C:\\temp\\tmpBG-from-NASA.jpg"
+$destPath = "C:\temp\pod2.jpg"
+
+$request =  "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
+
+$json = Invoke-WebRequest $request | ConvertFrom-Json
+$myurl = $json | select hdurl  | select -expand hdurl
+$Title = $json | select title  | select -expand title
+
+Invoke-WebRequest -Uri $myurl -OutFile $sourcePath
+$srcImg = [System.Drawing.Image]::FromFile($sourcePath)
+ 
+$bmpFile = new-object System.Drawing.Bitmap([int]($srcImg.width)),([int]($srcImg.height))
+
+$Image = [System.Drawing.Graphics]::FromImage($bmpFile)
+$Image.SmoothingMode = "AntiAlias"
+ 
+$Rectangle = New-Object Drawing.Rectangle 0, 0, $srcImg.Width, $srcImg.Height
+$Image.DrawImage($srcImg, $Rectangle, 0, 0, $srcImg.Width, $srcImg.Height, ([Drawing.GraphicsUnit]::Pixel))
+
+$Font = new-object System.Drawing.Font("Verdana", 80)
+$Brush = New-Object Drawing.SolidBrush ([System.Drawing.Color]::FromArgb(255, 255, 255, 255))
+$Image.DrawString($Title, $Font, $Brush, 10, 10)
+
+$bmpFile.save($destPath, [System.Drawing.Imaging.ImageFormat]::Jpeg)
+$bmpFile.Dispose()
+$srcImg.Dispose()
+
+set-wallPaper($destPath)
 
 
